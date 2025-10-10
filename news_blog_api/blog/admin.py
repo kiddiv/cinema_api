6559259bcbc127422ad26e044498a1c9f7d8a984
User <egorkathebestlordtop1gg@gmail.com>
2025-10-11 00:37:05 +0300

    fix

--- news_blog_api/blog/admin.py
+++ news_blog_api/blog/admin.py
@@ -17,10 +17,6 @@ class CommentAdmin(admin.ModelAdmin):
     search_fields = ['content']
     readonly_fields = ['created_at', 'updated_at']
 
-    def content_preview(self, obj):
-        return obj.content[:50] + '...' if len(obj.content) > 50 else obj.content
-
-    content_preview.short_description = 'Content Preview'
 
 
 @admin.register(Like)
