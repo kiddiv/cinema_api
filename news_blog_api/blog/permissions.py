feabb360941faf1a4db6077a70023fab55aaf4ec
User <egorkathebestlordtop1gg@gmail.com>
2025-10-19 14:21:16 +0300

    clear

--- news_blog_api/blog/permissions.py
+++
@@ -1,6 +0,0 @@
-from rest_framework.permissions import BasePermission
-class DeleteByAdmin(BasePermission):
-      def has_object_permission(self, request, view, obj):
-        if request.method in ['GET']:
-            return True
-        return obj.author == request.user or request.user.is_superuser
